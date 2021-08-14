a2d7226e399b2c24cb9feb5fe6b2e8e19a0da68c
khushikukreti <kukreti.khushi@gmail.com>
2021-08-15 00:14:53 +0530

    image div added

--- src/component/Loadingpage.js
+++ src/component/Loadingpage.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import './Loadingpage.css'
 
 function Loadingpage() {
     return (
-        <div>
-            Loading
+        <div className="main-container">
+            <div className="image1"> image1</div>
+            <div className="image2">imag2</div>
+            <div className="image3">image3</div>
+            <div className="image4">image4</div>
+            <div className="image5">image5</div>
         </div>
     )
 }
